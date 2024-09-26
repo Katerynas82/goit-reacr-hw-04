bf6a5e54311bba09b6b65d88ed8001181b610ed7
Kateryna Sukovaia <122564463+Katerynas82@users.noreply.github.com>
2024-09-26 23:04:52 +0100

    Fin

---
+++ src/Components/ImageModal/ImageModal.jsx
@@ -0,0 +1,31 @@
+import Modal from "react-modal";
+import style from "../ImageModal/ImageModal.module.css";
+
+const ImageModal = ({imageModal, setImageModal, largeImageUrl}) => {
+    
+    
+      function closeModal() {
+      setImageModal(false);
+    }
+  return (
+    <>
+      <div>
+        <Modal
+          isOpen={imageModal}
+          onRequestClose={closeModal}
+          style={style.content}
+          contentLabel="Image Modal"
+          shouldCloseOnOverlayClick={true}
+          shouldCloseOnEsc={true}
+        >
+          <div className={style.overlayModal}>
+            {largeImageUrl && <img src={largeImageUrl} alt="Large view" />}
+            <button onClick={closeModal}>close</button>
+          </div>
+        </Modal>
+      </div>
+    </>
+  );
+};
+
+export default ImageModal;
